a75b2ae7f8a566a9246d8e1c6e4a9f9e246e027d
flisochcs@gmail.com <flisochcs@gmail.com>
2022-06-14 22:29:19 +0300

    (#12) change project structure, add tests

--- test/maintest.cpp
+++ test/maintest.cpp
@@ -1,10 +1,5 @@
 #include "gtest/gtest.h"
 
-TEST(SquareRootTest, PositiveNos) { 
-    int i = 18;
-    EXPECT_EQ(18, i);
-}
-
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
